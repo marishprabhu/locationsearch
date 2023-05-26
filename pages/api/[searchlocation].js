04a4982afe30eee9390dc6079a3deb15b92df73d
mariscorenett <78922123+mariscorenett@users.noreply.github.com>
2023-05-27 00:56:58 +0530

    adding index

--- pages/api/[searchlocation].js
+++ pages/api/[searchlocation].js
@@ -1,18 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import Fuse from 'fuse.js';
-
 import location from './location.json';
+import fuseindex from './fuseindex.json';
 
 export default function handler(req, res) {
 	const { searchlocation } = req.query;
 	const options = {
 	minMatchCharLength: 3,
     threshold: 0.2,
-  keys: [
-    "c"
-  ]
-};
-	const fuse = new Fuse(location, options);
+    keys: [
+      "c"
+    ]
+  };
+  const fuse = new Fuse(location, options, Fuse.parseIndex(fuseindex));
   res.status(200).json(fuse.search(searchlocation));
-}
+ }
